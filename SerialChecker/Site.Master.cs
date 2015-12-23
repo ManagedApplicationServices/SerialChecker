0d3a25261f68756a72df4f30ced56291ba75ec81
zhaidy <dongyuzhai@gmail.com>
2015-12-23 08:42:02 +0800

    Remove session fullname

--- SerialChecker/Site.Master.cs
+++ SerialChecker/Site.Master.cs
@@ -10,21 +10,21 @@ namespace SerialChecker
 {
     public partial class SiteMaster : MasterPage
     {
-        public string fullname = "";
+        //public string fullname = "";
         protected void Page_Load(object sender, EventArgs e)
         {
-            if(!IsPostBack)
-            {
-                string user = Page.User.Identity.Name;
-                DirectorySearcher dirSearcher = new DirectorySearcher();
-                DirectoryEntry entry = new DirectoryEntry(dirSearcher.SearchRoot.Path);
-                dirSearcher.Filter = "(&(objectClass=user)(objectcategory=person)(samaccountname=" + user.Replace(@"RSP\", "") + "*))";
-                SearchResult srfullName = dirSearcher.FindOne();
-                string propName = "displayname";
-                ResultPropertyValueCollection valColl = srfullName.Properties[propName];
-                Session["fullname"] = Convert.ToString(valColl[0]);
-            }
-            fullname = Session["fullname"].ToString();
+            //if(!IsPostBack)
+            //{
+            //    string user = Page.User.Identity.Name;
+            //    DirectorySearcher dirSearcher = new DirectorySearcher();
+            //    DirectoryEntry entry = new DirectoryEntry(dirSearcher.SearchRoot.Path);
+            //    dirSearcher.Filter = "(&(objectClass=user)(objectcategory=person)(samaccountname=" + user.Replace(@"RSP\", "") + "*))";
+            //    SearchResult srfullName = dirSearcher.FindOne();
+            //    string propName = "displayname";
+            //    ResultPropertyValueCollection valColl = srfullName.Properties[propName];
+            //    Session["fullname"] = Convert.ToString(valColl[0]);
+            //}
+            //fullname = Session["fullname"].ToString();
         }
     }
 }
